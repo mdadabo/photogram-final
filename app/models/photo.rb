c47080a88a71d85e0a4f3246af5c28646d6e6191
mdadabo <162322810+mdadabo@users.noreply.github.com>
2024-08-16 17:02:23 +0000

    created associations, added sample data

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,4 +12,22 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
+
+  belongs_to(:owner,
+    class_name: "User",
+    foreign_key: "owner_id",
+    required: true
+  )
+
+  has_many(:comments,
+  class_name: "Comment",
+  foreign_key: "photo_id",
+  dependent: :destroy
+  )
+
+  has_many(:likes,
+    class_name: "Like",
+    foreign_key: "photo_id",
+    dependent: :destroy
+  )
 end
